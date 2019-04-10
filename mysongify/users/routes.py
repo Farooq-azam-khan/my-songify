bd9ca7d0640f1c6cb1e20f07a592c602536db221
Farooq Khan <farooq1.khan@ryerson.ca>
2019-04-10 16:03:26 -0700

    added login required

--- mysongify/users/routes.py
+++ mysongify/users/routes.py
@@ -1,12 +1,17 @@
 from flask import (Blueprint, 
                     render_template, 
                     flash)
-from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import (login_user, 
+                        current_user, 
+                        logout_user, 
+                        login_required)
 from mysongify.users.models import User 
 
 users = Blueprint('users', __name__)
 
+
 @users.route('/account')
+@login_required
 def account():
     return render_template('users/account.html')
 
@@ -15,13 +20,16 @@ def account():
 def login():
     login_user(User(0, 'admin', 'admin'))
     if current_user.is_authenticated:
-        flash('loggedin user', 'success')
+        flash('logged in user', 'success')
     return render_template('users/login.html')
 
 
 @users.route('/logout')
+@login_required
 def logout():
     logout_user()
+    if not current_user.is_authenticated:
+        flash('logged out', 'success')
     return render_template('users/logout.html')
 
 @users.route('/register')
