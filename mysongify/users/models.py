454aed1d5c822c12f48aeee3e02139f240488e4b
Farooq Khan <farooq1.khan@ryerson.ca>
2019-04-10 13:40:31 -0700

    added id field to user

--- mysongify/users/models.py
+++ mysongify/users/models.py
@@ -1,11 +1,13 @@
 class User():
-    def __init__(self, id, username, email, password):
-        self.id = id
-        self.username = username
+    id = 0
+    def __init__(self, email, password):
+        User.id += 1
         self.email = email
         self.password = password
         self.playlists = []
         self.image_file = 'default.png'
+        self.is_admin = False
+        self.is_dj = False
     
     def __repr__(self):
-        return f'<User {self.username}'
+        return f'<User {self.email}>'
