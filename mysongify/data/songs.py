0a726fa4709e50126b747cc55560ea7f6b50cfd4
Farooq Khan <farooq1.khan@ryerson.ca>
2019-04-10 13:20:34 -0700

    added methods

--- mysongify/data/songs.py
+++
@@ -1,8 +0,0 @@
-def get_songlist():
-	songslist = []
-	with open('songs.json') as f:
-		data = json.load(f)
-		songslist.extend(data[0]['songs'])
-		songslist.extend(data[1]['songs'])
-	return songslist
-    
