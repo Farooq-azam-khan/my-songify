66d2e1db66438a1568c538bca9c7339171d4754d
Farooq Khan <farooq1.khan@ryerson.ca>
2019-04-10 16:30:01 -0700

    added form playlist

--- mysongify/playlists/routes.py
+++ mysongify/playlists/routes.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template
-
+from flask import (Blueprint, 
+                    render_template, 
+                    redirect,
+                    request, 
+                    flash,
+                    url_for
+                    )
+from flask_login import login_required
 
 playlists = Blueprint('playlists', __name__)
 
-@playlists.route('/create_playlist')
+@playlists.route('/create_playlist', methods=['POST', 'GET'])
+@login_required
 def create_playlist():
+    if request.method == 'POST':
+        flash('post sending data', 'primary')
+        return redirect(url_for('main.home'))
+    elif request.method == 'GET':
+        flash('get request', 'success')
     return render_template('playlists/create_playlist.html')
