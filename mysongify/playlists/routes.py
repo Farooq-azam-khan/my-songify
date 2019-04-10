9afc6eef10b4e667788fb4a82b297d72fa9ba3c0
Farooq Khan <farooq1.khan@ryerson.ca>
2019-04-10 16:39:15 -0700

    songs on create playlsit

--- mysongify/playlists/routes.py
+++ mysongify/playlists/routes.py
@@ -6,15 +6,16 @@ from flask import (Blueprint,
                     url_for
                     )
 from flask_login import login_required
-
+from mysongify.songs.models import Song
 playlists = Blueprint('playlists', __name__)
 
 @playlists.route('/create_playlist', methods=['POST', 'GET'])
 @login_required
 def create_playlist():
+    songs = Song.get_songlist()[:10]
     if request.method == 'POST':
         flash('post sending data', 'primary')
         return redirect(url_for('main.home'))
     elif request.method == 'GET':
         flash('get request', 'success')
-    return render_template('playlists/create_playlist.html')
+    return render_template('playlists/create_playlist.html', songs=songs)
