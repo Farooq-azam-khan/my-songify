73a0372405685289c02794f313ae98ac66fb220b
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-04 04:30:32 -0400

    added utcnow

--- backend/app/models.py
+++ backend/app/models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    added_at = db.Column(db.DateTime, default=datetime.now)
+    added_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.pk} - {self.email}>'
