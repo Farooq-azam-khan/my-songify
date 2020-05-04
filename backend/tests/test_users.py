b5650ab41c0e7e02b7ca3106cc78657d94a7be02
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-04 06:40:35 -0400

    created tests for users

---
+++ backend/tests/test_users.py
@@ -0,0 +1,30 @@
+from app.models import User
+from app import db 
+
+def test_user_id(app):
+    user = User(email='testing@id.com', firstname='fn', lastname='ln')
+    user.set_password('password')
+
+    db.session.add(user)
+
+    db.session.commit()
+
+    query_user = User.query.first()
+
+    assert int(query_user.pk) == 1
+    assert query_user.email == 'testing@id.com'
+    assert query_user.check_password('password') == True
+
+    assert query_user.password != "password"
+    assert query_user.check_password("not test") == False
+
+    assert query_user.is_admin == False
+
+
+
+    
+def test_users_route(app):
+    client = app.test_client()
+    resp = client.get('/users/')
+    assert resp.status_code == 200
+
