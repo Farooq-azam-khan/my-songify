556590fb975785070e2fe3bf9a7f86685e302597
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-04 05:32:52 -0400

    created app factory

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,3 @@
-#TODO: implement Blueprint
 
 from flask import Flask
 from flask_login import LoginManager
@@ -7,15 +6,42 @@ from flask_migrate import Migrate
 
 from config import Config
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 # configuration
-app.config.from_object(Config)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+
+# application factory
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    initalize_extensions(app)
+
+    register_blueprints(app)
+
+    return app
+
+def initalize_extensions(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+def register_blueprints(app):
+    from app.users import users_blueprint
+    from app.main import main_blueprint 
+
+    app.register_blueprint(users_blueprint)
+    app.register_blueprint(main_blueprint)
+
+'''app.config.from_object(Config)
 # database connection
 db = SQLAlchemy(app)
 # migration connection
 migrate = Migrate(app, db)
 # login manager
 login = LoginManager(app)
-
-from app import routes, models 
+'''
+#from app import routes, models 
--- backend/application.py
+++ backend/application.py
@@ -1,5 +1,8 @@
-from app import app
+from app import create_app 
 from app.models import db, User
+from config import Config
+
+app = create_app(Config)
 
 # add a context when the `flask shell` command is run
 @app.shell_context_processor
