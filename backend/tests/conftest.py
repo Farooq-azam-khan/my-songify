b32c324f82d9d85aa478c62a18a7c2332e89b7e7
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-04 06:40:20 -0400

    cearted test app and test database

---
+++ backend/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from app import create_app, db
+from app.models import User
+from test_config import TestConfig
+
+
+# create a user to test its functionality
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(email='testuser@my_songify.com', firstname='first', lastname='user')
+    user.set_password('admin')
+    return user
+
+# https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
+@pytest.fixture
+def app():
+    app = create_app(TestConfig)
+    with app.app_context():   
+        db.create_all()
+        yield app  
+        db.session.remove() 
+        db.drop_all()
