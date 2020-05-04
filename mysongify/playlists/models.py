59bebdf78bfabc0addd580311ad0d4c9dc708540
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-03 21:49:49 -0400

    removed v1 code

--- mysongify/playlists/models.py
+++
@@ -1,5 +0,0 @@
-from mysongify.songs.models import Song 
-
-
-class Playlist():
-    pass
--- mysongify/users/models.py
+++
@@ -1,6 +0,0 @@
-from flask_login import UserMixin, current_user 
-from mysongify import login_manager
-
-
-class User(UserMixin):
-    pass
