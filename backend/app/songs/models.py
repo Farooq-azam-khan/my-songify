b5bbeff8b020fd00d1a6cfc05602d9a76ea9e3a7
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-04 23:08:47 -0400

    added unique to models and removed migrations folder for repo

--- backend/app/songs/models.py
+++ backend/app/songs/models.py
@@ -20,7 +20,7 @@ class Song(db.Model):
 
 class Genre(db.Model):
     pk = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
 
     @staticmethod 
     def add_default_genres():
