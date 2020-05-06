b6f4d60c5cba88cf1380f8d1668db90df14f5529
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-06 00:11:42 -0400

    created navbar with about and login route

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,18 +4,17 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-
-import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
+  },
+  bgr: {
+    background: "red",
   },
 }));
 
@@ -23,16 +22,17 @@ const Navbar = () => {
   const classes = useStyles();
 
   return (
-    <Grid item className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            MY-SONGIFY
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Grid>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          MY-SONGIFY
+        </Typography>
+        <Button href="#about" color="inherit">
+          About
+        </Button>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
