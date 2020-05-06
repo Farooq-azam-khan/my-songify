8ea2a919e82944550877c346ac3de4864e10e449
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-06 00:11:59 -0400

    created home page

---
+++ frontend/src/components/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flex: 1,
+    marginTop: "2rem",
+    padding: "2rem",
+    color: "white",
+    backgroundColor: "black",
+  },
+  smallCaps: {
+    fontVaiant: "small-caps",
+  },
+}));
+
+const About = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      className={classes.root}
+      container
+      direction="column"
+      justify="center"
+      align="center"
+    >
+      <Grid item xs={12}>
+        <Typography className={classes.smallCaps} variant="h3">
+          About
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography id="about" variant="subtitle1" element="p">
+          This project has been created to test the insides of websites like
+          Tidal and Spotify which are used for listening to songs.
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/AlbumCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+const AlbumCard = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="subtitle1" component="h2">
+            Album Name
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default AlbumCard;
