49cef7e5bd9804b43d337f4aaac4fd89599b2e6c
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-09 03:44:41 -0400

    created a store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,44 +3,46 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Navbar from "./components/Navbar";
 import Grid from "@material-ui/core/Grid";
 
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Songs from "./pages/Songs";
 import Register from "./pages/Register";
-
-import "./App.css";
+import store from "./store";
 
 const App = () => {
   return (
-    <div className="App">
-      <CssBaseline />
-      <Router>
-        <Grid container direction="column">
-          <Grid item style={{ margin: 0 }}>
-            <Navbar />
-          </Grid>
-          <Grid item>
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/songs">
-                <Songs />
-              </Route>
+    <Provider store={store}>
+      <div className="App">
+        <CssBaseline />
+        <Router>
+          <Grid container direction="column">
+            <Grid item style={{ margin: 0 }}>
+              <Navbar />
+            </Grid>
+            <Grid item>
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/songs">
+                  <Songs />
+                </Route>
 
-              <Route exact path="/login">
-                <Login />
-              </Route>
-              <Route exact path="/register">
-                <Register />
-              </Route>
-            </Switch>
+                <Route exact path="/login">
+                  <Login />
+                </Route>
+                <Route exact path="/register">
+                  <Register />
+                </Route>
+              </Switch>
+            </Grid>
           </Grid>
-        </Grid>
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </Provider>
   );
 };
 
