9badbd07e107875ffde466cdde6b1c78c20e2c4d
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-08 23:22:30 -0400

    loing and logout route

--- backend/app/users/routes.py
+++ backend/app/users/routes.py
@@ -1,18 +1,20 @@
 from flask import jsonify, request 
-from flask_login import logout_user, current_user, login_user
+from flask_login import logout_user, current_user, login_user, login_required
 
 from . import users_blueprint
 from app.users.models import User
+from app.users.forms.login_form import LoginForm
 
 @users_blueprint.route('/users/')
 def index():
     return jsonify({'users': ['user 1']})
 
 # logout
-@users_blueprint.route('/users/logout')
+@users_blueprint.route('/users/logout', methods=['POST'])
+@login_required
 def logout():
     logout_user()
-    return jsonify({'success':True})
+    return jsonify({'success':True, 'message': 'You were logged out successfully'})
 
 
 
@@ -24,13 +26,12 @@ def login():
     
     email = request.form.get('email')
     password = request.form.get('password')
+
     remember = request.form.get('remember')
-    # print('users/router.py/login:', email, password)
     user = User.query.filter_by(email=email).first()
     
     if user is None or not user.check_password(password):
         return jsonify({'success': False, 'message': 'Could not login','errors': ['Invalid email or password']})
     
-    # print('routes/', user)
-    login_user(user) #, remember=remember)
+    login_user(user, remember=remember)
     return jsonify({'success': True, 'message': 'Logged in successfully.', 'errors': [], 'todo': 'implement login'})
