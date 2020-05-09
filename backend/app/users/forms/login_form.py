a0011e035a69f6ff9442c5220384c25bcca4a771
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-09 00:06:17 -0400

    fixed bug

--- backend/app/users/forms/login_form.py
+++ backend/app/users/forms/login_form.py
@@ -2,7 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField 
 from wtforms.validators import DataRequired, Email
 from wtforms.fields.html5 import EmailField
+from wtforms.fields import PasswordField
 
 class LoginForm(FlaskForm):
     email = EmailField('email', validators=[Email(), DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
