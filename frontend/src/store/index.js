e65f4fcbb6e88b12a62d0d2a6be240574fbc6492
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-09 06:56:02 -0400

    created a redux store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
-const store = createStore(() => [], {}, applyMiddleware());
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import rootReducer from "./reducers";
+
+const initial_state = {};
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initial_state,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
