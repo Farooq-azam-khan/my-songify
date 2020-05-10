666fbf217e9a7946758da834d84ed24166498df8
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-10 00:02:15 -0400

    updated route for getting songs by genre

--- backend/app/songs/routes.py
+++ backend/app/songs/routes.py
@@ -11,12 +11,12 @@ from .models import Genre, Song, UserSongRelationship
 @songs_blueprint.route('/songs/')
 def songs():
     genres = Genre.query.all()
-    ret = {}
+    data = {}
     for genre in genres:
         genre_name = str(genre.name)
-        ret[genre_name] = [song.name for song in Song.query.filter_by(genre=genre.pk).all()]
+        data[genre_name] = [song.get_song_dict() for song in Song.query.filter_by(genre=genre.pk).all()]
 
-    return jsonify(ret)
+    return jsonify({'success': True, 'message': 'List of songs grouped by genre', 'data': data})
 
 @songs_blueprint.route('/genres/')
 def genres():
