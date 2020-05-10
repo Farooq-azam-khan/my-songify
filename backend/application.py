cc5e55efcb0c2f1fea17c50ffc66b8085e5fcef1
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-09 22:23:36 -0400

    Update application.py

--- backend/application.py
+++ backend/application.py
@@ -1,11 +1,12 @@
 from app import create_app 
 from app import db
-from app.songs.models import Song, Genre
+from app.songs.models import Song, Genre, UserSongRelationship
 from app.users.models import User
 from app.song_collection.models import (SongCollection, 
                                         Playlist, Album, 
                                         DisplayStatus, SongList
                                         )
+
 from config import Config
 
 app = create_app(Config)
@@ -13,10 +14,10 @@ app = create_app(Config)
 # add a context when the `flask shell` command is run
 @app.shell_context_processor
 def make_shell_context():
-    # sample_user = User(firstname='f1', lastname='ln', email='f@l.com')
-    # sample_user.set_password('test')
-    # db.session.add(sample_user)
-    # db.session.commit()
+# sample_user = User(firstname='f1', lastname='ln', email='f@l.com')
+# sample_user.set_password('test')
+# db.session.add(sample_user)
+# db.session.commit()
 
     # sample_song = Song(name='song1', mp3_file='google.com')
     # db.session.add(sample_song)
@@ -24,7 +25,11 @@ def make_shell_context():
     # db.session.add(sample_song)
     # db.session.commit()
 
-    return {'db': db, 'User': User,
+    # statement = user_song_relationship.insert().values(user=1, song=1, is_like=True)
+    # db.session.execute(statement)
+    # db.session.commit()
+
+    return {'db': db, 'User': User, 'UserSongRelationship': UserSongRelationship,
     'Song': Song, 'Genre': Genre, 
     'SongCollection':SongCollection, 'Playlist':Playlist, 'Album':Album, 
     'DisplayStatus': DisplayStatus, 'SongList': SongList}
