67ee38e348b363fc0ae75fd9a92cad52a6c151d5
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-09 23:34:18 -0400

    added tests and modifications to like_song route

--- backend/app/songs/routes.py
+++ backend/app/songs/routes.py
@@ -1,7 +1,12 @@
-from flask import jsonify
+from flask import jsonify, request
+from flask_login import current_user, login_required
+
 from . import songs_blueprint
 from app import db 
-from .models import Genre, Song
+from .models import Genre, Song, UserSongRelationship 
+
+
+
 
 @songs_blueprint.route('/songs/')
 def songs():
@@ -16,3 +21,22 @@ def songs():
 @songs_blueprint.route('/genres/')
 def genres():
     return jsonify([genre.name for genre in Genre.query.all()])
+
+
+@songs_blueprint.route('/songs/like', methods=['POST'])
+@login_required
+def like_song():
+    # current user 
+    song = request.form.get('song') # expecting id 
+    is_like = request.form.get('is_like') #expecting boolean
+    try:
+        UserSongRelationship.add_entry(user=current_user.pk, song=song, is_like=is_like)
+        return jsonify({'success': True, 'message': 'added like/dislike to database'})
+    except:
+        return jsonify({'success': False, 'message': 'no update or addition'})
+
+
+
+# @songs_blueprint.route('/genres/<genre_id>')
+# def genre_list(genre_id):
+#     pass 
--- backend/tests/songs/test_user_song_relationship.py
+++ backend/tests/songs/test_user_song_relationship.py
@@ -1,3 +1,5 @@
+import json 
+
 import pytest
 
 from app.songs.models import Song, Genre, UserSongRelationship
@@ -53,4 +55,56 @@ def test_add_entry_user_song_relationship_duplicate_addition(app):
     
     print(execinfo)
     assert 'Cannot add multiple rows with the same primary key' in str(execinfo.value)
+
+# user relationship routes 
+def test_like_song_valid_post_form(app):
+    user = User(email='tst@tst.com', firstname='f', lastname='l')
+    user.set_password('password')
+    
+    song = Song(name='song1', user=1, mp3_file='google.com')
+
+    db.session.add(user)
+    db.session.add(song)
+    db.session.commit()
+
+    with app.test_client() as client:
+        # login in uer 
+        resp = client.post('/users/login', data=dict(email='tst@tst.com', password='password', remember=False)) 
+        assert resp.status_code == 200
+
+        resp = client.post('/songs/like', data=dict(song=1, is_like=True))
+        assert resp.status_code == 200
+        assert json.loads(resp.data)['success'] == True
+        assert json.loads(resp.data)['message'] == 'added like/dislike to database'
+
+def test_like_song_does_not_update_like(app):
+    user = User(email='tst@tst.com', firstname='f', lastname='l')
+    user.set_password('password')
+    
+    song = Song(name='song1', user=1, mp3_file='google.com')
+
+    db.session.add(user)
+    db.session.add(song)
+    db.session.commit()
+
+    with app.test_client() as client:
+        # login in uer 
+        resp = client.post('/users/login', data=dict(email='tst@tst.com', password='password', remember=False)) 
+        assert resp.status_code == 200
+
+        resp = client.post('/songs/like', data=dict(song=1, is_like=True))
+        assert resp.status_code == 200
+        assert json.loads(resp.data)['success'] == True
+        assert json.loads(resp.data)['message'] == 'added like/dislike to database'
+
+        resp = client.post('/songs/like', data=dict(song=1, is_like=True))
+        assert resp.status_code == 200
+        assert json.loads(resp.data)['success'] == False
+        assert json.loads(resp.data)['message'] == 'no update or addition'
+
+def test_like_song_login_required(app):
+    with app.test_client() as client:
+      
+        resp = client.post('/songs/like', data=dict(song=1, is_like=True))
+        assert resp.status_code == 401
 
