fcab7fed6064a3b338981ae7d5ae4bc05b155044
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-15 12:48:33 -0400

    Updates

---
+++ precache-manifest.20401db2eecc892994654dd46d910abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4caf31526d479cd6687abb827299fa4",
+    "url": "/my-songify/index.html"
+  },
+  {
+    "revision": "3eba065c778274787c8a",
+    "url": "/my-songify/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d568e6ebc49a1cd9f9d4",
+    "url": "/my-songify/static/js/2.36926348.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-songify/static/js/2.36926348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eba065c778274787c8a",
+    "url": "/my-songify/static/js/main.28ae1cc4.chunk.js"
+  },
+  {
+    "revision": "21861290dba7645a3be3",
+    "url": "/my-songify/static/js/runtime-main.1d7d35be.js"
+  }
+]);
