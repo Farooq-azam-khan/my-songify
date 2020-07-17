077a3fb7f8bf5964748e2bde57f00fe1ca952ca2
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-07-17 17:16:00 -0400

    removed flask example, added styles for home page

--- app/static/Home.jsx
+++ app/static/Home.jsx
@@ -1,3 +1,18 @@
 const Home = () => {
-    return <div>home</div>
+    return <div className="bg-gray-900 w-full h-full">
+        <div className="px-10 py-2">
+            <h1 className="text-teal-100 font-bold text-4xl text-center mt-10">My Songify</h1>
+            <p className="text-white mt-10">Amet anim aliqua amet laboris culpa deserunt ad id et magna voluptate voluptate. Culpa deserunt ad culpa labore sit ea cillum ullamco. Nisi consequat do ea sunt. Dolore Lorem id est fugiat proident consectetur non do. Culpa veniam enim nulla amet ea dolore officia fugiat nulla culpa et est.
+In amet duis mollit minim reprehenderit. Ex ad sint aliquip magna. Lorem reprehenderit incididunt quis ad fugiat exercitation excepteur velit elit fugiat occaecat eiusmod consectetur.</p>
+        </div>
+        <div className="flex items-center justify-between">
+            <div className="border-l-2 border-teal-500">
+                <h2 className="font-bold uppercase text-white text-4xl">Keep Track<br /> of the <br />
+                    <span className="text-teal-500">Best Music</span> in the <br /> <span className="text-teal-500">Industry</span>
+                </h2>
+            </div>
+
+        </div>
+
+    </div>
 }
