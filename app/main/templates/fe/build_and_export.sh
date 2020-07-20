0ff846278c862736ef82279f20c03e7d524459f4
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-07-20 15:59:09 -0400

    added sed command to enable and disable purgecss before and after build respectively

--- app/main/templates/fe/build_and_export.sh
+++ app/main/templates/fe/build_and_export.sh
@@ -1,3 +1,5 @@
+sed -i "s/enabled: false/enabled: true/g" tailwind.config.js
 npm run build:tailwind
+sed -i "s/enabled: true/enabled: false/g" tailwind.config.js
 npm run build
 mv build ../../../static/
