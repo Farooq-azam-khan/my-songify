5e49b68cf053cee901e448b2e1f3b1d75091b8d4
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-07-22 12:54:40 -0400

    Update Profile.js
    commented out some unused vars and imports

--- app/main/templates/fe/src/pages/Profile.js
+++ app/main/templates/fe/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import * as Icons from 'heroicons-react'
+// import * as Icons from 'heroicons-react'
 
 import UserCreatedPlaylists from '../components/UserCreatedPlaylists';
 
@@ -44,35 +44,35 @@ const Profile = ({ user }) => {
 
 
 // TODO: needs serious refactoring
-const CollectionDisplay = ({ collection, collectionTitle = "", nullMessage, isPlaylist = true, onClick, children, loadingCollection = false }) => {
-    return <div className="bg-gray-800 rounded-lg shadow-lg max-w-3xl mt-10">
-        {loadingCollection ? <div className="">
-            <h2 className="text-white bg-teal-800 text-xl">Loading...</h2></div> : <>
-                {children}
-                {Object.keys(collection).length === 0 ? <div className="flex flex-col items-center space-y-5 mt-10 py-10 px-12 rounded-lg shadow-lg">
-                    <button onClick={onClick} className={`flex items-center justify-center px-3 py-2 max-w-xl space-x-3 rounded-lg shadow-md text-white ${isPlaylist ? 'bg-teal-800 hover:bg-teal-700' : 'bg-blue-800 hover:bg-blue-700'} `}>
-                        <span className="text-white"><Icons.Plus /></span>
-                        <span>{isPlaylist ? 'Playlist' : 'Album'}</span></button>
-                    <p className="text-white font-semibold tracking-wide">{nullMessage}</p>
-                </div> :
-                    <div className="flex flex-col space-y-3 items-center justify-center py-2 ">
-                        <div className="flex items-center justify-between w-full px-3">
-                            {collectionTitle !== "" && <span className="w-full text-center font-bold text-orange-500 uppercase tracking-wider text-xl">{collectionTitle}</span>}
-                            <button onClick={onClick} className="inline-flex items-center justify-between bg-teal-800 text-white text-xs rounded-full px-2 py-1"><span><Icons.PlusOutline className="text-gray-400 w-4 h-4" /></span> <span>Playlist</span></button>
-                        </div>
-                        <div className="flex items-center justify-around space-x-3 overflow-scroll overflow-y-hidden w-full h-64">
-                            {Object.keys(collection).map(el => <div key={el} class="text-white rounded-lg shadow-md flex flex-col overflow-hidden w-64 h-full bg-red-700">
-                                <div className="overflow-hidden">
-                                    <img className="w-64 h-64 object-cover object-center" src={collection[el].cover_image} alt={el} />
-                                </div>
-                                <div className="text-white bg-blue-800 text-md uppercase tracking-wider w-full text-center">{el}</div>
-                            </div>)}
-                        </div>
-                    </div>
-                }
-            </>}
-    </div>
-}
+// const CollectionDisplay = ({ collection, collectionTitle = "", nullMessage, isPlaylist = true, onClick, children, loadingCollection = false }) => {
+//     return <div className="bg-gray-800 rounded-lg shadow-lg max-w-3xl mt-10">
+//         {loadingCollection ? <div className="">
+//             <h2 className="text-white bg-teal-800 text-xl">Loading...</h2></div> : <>
+//                 {children}
+//                 {Object.keys(collection).length === 0 ? <div className="flex flex-col items-center space-y-5 mt-10 py-10 px-12 rounded-lg shadow-lg">
+//                     <button onClick={onClick} className={`flex items-center justify-center px-3 py-2 max-w-xl space-x-3 rounded-lg shadow-md text-white ${isPlaylist ? 'bg-teal-800 hover:bg-teal-700' : 'bg-blue-800 hover:bg-blue-700'} `}>
+//                         <span className="text-white"><Icons.Plus /></span>
+//                         <span>{isPlaylist ? 'Playlist' : 'Album'}</span></button>
+//                     <p className="text-white font-semibold tracking-wide">{nullMessage}</p>
+//                 </div> :
+//                     <div className="flex flex-col space-y-3 items-center justify-center py-2 ">
+//                         <div className="flex items-center justify-between w-full px-3">
+//                             {collectionTitle !== "" && <span className="w-full text-center font-bold text-orange-500 uppercase tracking-wider text-xl">{collectionTitle}</span>}
+//                             <button onClick={onClick} className="inline-flex items-center justify-between bg-teal-800 text-white text-xs rounded-full px-2 py-1"><span><Icons.PlusOutline className="text-gray-400 w-4 h-4" /></span> <span>Playlist</span></button>
+//                         </div>
+//                         <div className="flex items-center justify-around space-x-3 overflow-scroll overflow-y-hidden w-full h-64">
+//                             {Object.keys(collection).map(el => <div key={el} class="text-white rounded-lg shadow-md flex flex-col overflow-hidden w-64 h-full bg-red-700">
+//                                 <div className="overflow-hidden">
+//                                     <img className="w-64 h-64 object-cover object-center" src={collection[el].cover_image} alt={el} />
+//                                 </div>
+//                                 <div className="text-white bg-blue-800 text-md uppercase tracking-wider w-full text-center">{el}</div>
+//                             </div>)}
+//                         </div>
+//                     </div>
+//                 }
+//             </>}
+//     </div>
+// }
 
 
 
