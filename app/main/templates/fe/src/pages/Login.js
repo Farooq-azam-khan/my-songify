f66fb0e7025fdc9088be4f078e0c8311963ab4a4
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-07-22 12:54:22 -0400

    Update Login.js
    added useForm state instead of useState for email and password

--- app/main/templates/fe/src/pages/Login.js
+++ app/main/templates/fe/src/pages/Login.js
@@ -6,21 +6,14 @@ import { connect } from 'react-redux';
 import { loginAction } from '../store/actions/userActions'
 
 const Login = ({ user, loginAction }) => {
-    const { register, handleSubmit, watch, errors } = useForm();
+    const { register, handleSubmit, errors } = useForm();
 
     const onSubmit = data => {
+        const { email, password } = data;
         loginAction(email, password)
 
     }
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
-    }
 
     if (user.loggedIn) {
         return (<div className="fixed z-10 inset-0 flex items-center justify-center bg-gray-900 h-full">
@@ -31,10 +24,20 @@ const Login = ({ user, loginAction }) => {
     return (<div className="fixed z-10 inset-0 flex flex-col items-center justify-center w-full h-full bg-gray-900">
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center max-w-xl mx-auto  space-y-2 mt-5 w-full h-full px-3 sm:px-10">
             <div className="flex flex-col sm:flex-row space-x-0 space-y-1 sm:space-y-0 items-center w-full sm:space-x-4 justify-between">
-                <input value={email} onChange={handleEmail} className="w-full bg-teal-100 px-3 py-2 rounded-lg" type="email" placeholder="Email" />
+                <div className="w-full flex flex-col space-y-2">
+                    <input name="email" ref={register({ required: true })}
+                        className="w-full bg-teal-100 px-3 py-2 rounded-lg" type="email" placeholder="Email"
+                    />
+                    {errors.email && <div className="rounded-lg bg-red-300 text-gray-900 px-3 py-2">Email Field is required.</div>}
+                </div>
             </div>
             <div className="flex flex-col sm:flex-row space-x-0 space-y-1 sm:space-y-0 items-center w-full sm:space-x-4 justify-between">
-                <input value={password} onChange={handlePassword} className="w-full bg-teal-100 px-3 py-2 rounded-lg" type="password" placeholder="Password" />
+                <div className="w-full flex flex-col space-y-2">
+                    <input name="password" ref={register({ required: true })}
+                        className="w-full bg-teal-100 px-3 py-2 rounded-lg" type="password" placeholder="Password"
+                    />
+                    {errors.password && <div className="rounded-lg bg-red-300 text-gray-900 px-3 py-2">Email Field is required.</div>}
+                </div>
             </div>
             <button type="submit" className="block bg-teal-700 text-white rounded-lg shadow-lg text-md px-10 py-3">Login</button>
         </form>
